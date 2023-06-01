4d207408910530d237ca42dc35f86549bf49c47f
mohit-sculptsoft <131239259+mohit-sculptsoft@users.noreply.github.com>
2023-06-01 19:38:26 +0530

    Update run-test.py

--- run-test.py
+++ run-test.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+import time
 
 # Create a new Firefox driver
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
 
 # Open the test file
 driver.get("./testing.side")
